0e70f353dc5f0f2f811c86369c6383890b8f26bc
ALRT <lopanov.arseniy@outlook.com>
2024-09-18 21:58:09 +0300

    Консольное приложение Архив заметок (Исправленный)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,23 +3,23 @@ import java.util.Scanner
 fun main() {
     val menuHandler = MenuHandler()
     val archives = mutableListOf<Archive>()
-    1
-    menuHandler.showMenu("Меню архивов", listOf("Создать архив" to {
-        println("Введите название архива:")
-        val name = readNonEmptyInput()
-        archives.add(Archive(name))
-        println("Архив '$name' создан.")
-    }, "Просмотреть архивы" to {
-        if (archives.isEmpty()) {
-            println("Нет доступных архивов.")
-        } else {
-            menuHandler.showMenu(
-                "Выбор архива",
-                archives.map { it.name to { it.viewNotes(menuHandler) } })
-        }
-    }))
-}
 
+    menuHandler.showMenu("Меню архивов", listOf(
+        "Создать архив" to {
+            println("Введите название архива:")
+            val name = readNonEmptyInput()
+            archives.add(Archive(name))
+            println("Архив '$name' создан.")
+        },
+        "Просмотреть архивы" to {
+            if (archives.isEmpty()) {
+                println("Нет доступных архивов.")
+            } else {
+                menuHandler.showMenu("Выбор архива", archives.map { it.name to { it.archiveMenu(menuHandler) } })
+            }
+        }
+    ))
+}
 
 fun readNonEmptyInput(): String {
     while (true) {
