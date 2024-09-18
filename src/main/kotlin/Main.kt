5796d89cd71b774c80b9cb2a526a629d0749c246
ALRT <lopanov.arseniy@outlook.com>
2024-09-18 21:55:13 +0300

    Консольное приложение(архив)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,30 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.Scanner
+
+fun main() {
+    val menuHandler = MenuHandler()
+    val archives = mutableListOf<Archive>()
+    1
+    menuHandler.showMenu("Меню архивов", listOf("Создать архив" to {
+        println("Введите название архива:")
+        val name = readNonEmptyInput()
+        archives.add(Archive(name))
+        println("Архив '$name' создан.")
+    }, "Просмотреть архивы" to {
+        if (archives.isEmpty()) {
+            println("Нет доступных архивов.")
+        } else {
+            menuHandler.showMenu(
+                "Выбор архива",
+                archives.map { it.name to { it.viewNotes(menuHandler) } })
+        }
+    }))
+}
+
+
+fun readNonEmptyInput(): String {
+    while (true) {
+        val input = Scanner(System.`in`).nextLine()
+        if (input.isNotBlank()) return input
+        else println("Поле не может быть пустым. Попробуйте снова:")
+    }
 }
